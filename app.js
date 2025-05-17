08095a10e1534f0d804a58053132fd0056eb03e6
sbleyl <144071360+sbleyl@users.noreply.github.com>
2025-05-16 21:59:49 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -10,11 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-<<<<<<< HEAD
-const PORT = 2992;
-=======
 const PORT = 2998;
->>>>>>> 185142f (Initial commit on sbleyl branch)
 
 // Database for CRUD actions on the data
 const db = require('./database/db-connector.js');
@@ -67,20 +63,11 @@ app.get('/customers', async function (req, res) {
 //retrieve a list of our current products
 app.get('/products', async function (req, res) {
     try {
-<<<<<<< HEAD
         // returns all attributes on the product
         const query1 = `SELECT \
             Products.productName AS 'name', \
             Products.productType AS 'type', \
             Products.unitPrice AS 'price' \ 
-=======
-        // Fixed SQL query - removed trailing space after 'price' \
-        const query1 = `SELECT 
-            Products.productID AS 'id', \
-            Products.productName AS 'name',
-            Products.productType AS 'type',
-            Products.unitPrice AS 'price'
->>>>>>> 185142f (Initial commit on sbleyl branch)
             FROM Products;`;
         const [products] = await db.query(query1);
 
@@ -88,18 +75,9 @@ app.get('/products', async function (req, res) {
         res.render('products', { products: products});
     } catch (error) {
         console.error('Error executing queries:', error);
-<<<<<<< HEAD
         // Send a generic error message to the browser
         res.status(500).send(
             "We're sorry, we've hit an error on our end. Please check back in a few minutes"
-=======
-        console.error('Error details:', error.code, error.message);
-        
-        // Send a more informative error message
-        res.status(500).send(
-            "Database connection error (code: " + (error.code || "unknown") + "). " +
-            "Please try again later or contact your instructor."
->>>>>>> 185142f (Initial commit on sbleyl branch)
         );
     }
 });
@@ -112,20 +90,12 @@ app.get('/locations', async function (req, res) {
     try {
         // returns relevant attributes on locations
         const query1 = `SELECT  \
-<<<<<<< HEAD
+        Locations.locationID as 'id', \
         Locations.locationName as 'branch', \
         Locations.locationAddress as 'address', \
         Locations.locationCity AS 'city', \
         Locations.locationStateAbbr AS 'state', \
         Locations.phone AS 'phone' FROM Locations;`;
-=======
-            Locations.locationID AS 'id',
-            Locations.locationName AS 'name',
-            Locations.locationAddress AS 'address',
-            Locations.locationCity AS 'city',
-            Locations.locationStateAbbr AS 'state',
-            Locations.phone AS 'phone' FROM Locations;`;
->>>>>>> 185142f (Initial commit on sbleyl branch)
         const [locations] = await db.query(query1);
 
         // Render the Customer Table inside of the customer.hbs file
@@ -144,8 +114,8 @@ app.get('/locations', async function (req, res) {
 app.get('/employees', async function (req, res) {
     try {
         // returns relevant attributes on locations
-<<<<<<< HEAD
         const query1 = `SELECT  \
+        Employees.employeeID as 'id', \
         Employees.firstName as 'first', \
         Employees.lastName as 'last', \
         Employees.employeeRole AS 'role', \
@@ -156,30 +126,11 @@ app.get('/employees', async function (req, res) {
         ORDER BY branch`;
         const [employees] = await db.query(query1);
 
-        // Render the employees Table inside of the employees.hbs file
-        res.render('employees', { employees: employees});
-=======
-        const query1 = `SELECT  
-            Employees.employeeID           AS id,
-            Employees.firstName            AS first,
-            Employees.lastName             AS last,
-            Employees.phone                AS phone,
-            Employees.email                AS email,
-            Employees.employeeAddress      AS address,
-            Employees.employeeCity         AS city,
-            Employees.employeeStateAbbr    AS state,
-            Employees.locationID           AS locationID,
-            Employees.ordersFulfilledCount AS fulfilled,
-            Employees.ordersActiveCount    AS active,
-            Employees.employeeRole         AS role
-        FROM Employees
-        JOIN Locations ON Employees.locationID = Locations.locationID
-        ORDER BY Employees.employeeID;`;
-        const [employees] = await db.query(query1);
+        const query2 =`SELECT * FROM Locations`;
+        const [locations] = await db.query(query2);
 
         // Render the employees Table inside of the employees.hbs file
-        res.render('employees', { employees });
->>>>>>> 185142f (Initial commit on sbleyl branch)
+        res.render('employees', { employees: employees, locations: locations});
     } catch (error) {
         console.error('Error executing queries:', error);
         // Send a generic error message to the browser
@@ -194,55 +145,44 @@ app.get('/orders', async function (req, res) {
     try {
         // returns relevant attributes on locations
         const query1 = `SELECT  \
-<<<<<<< HEAD
         Orders.orderID as 'id', \
         Customers.firstName as 'customerFirst', \
         Customers.lastName as 'customerLast', \
         Employees.lastName as 'employee', \
         Locations.locationName AS 'branch', \
-        Orders.dateEstimateDelivery AS 'estimate', \
+        DATE_format(Orders.dateEstimateDelivery, "%m/%e/%y") AS 'estimate', \
         Orders.orderStatus AS 'status', \
         Orders.subtotal AS 'subtotal', \
         Orders.tax AS 'tax', \
-        Orders.orderTotal AS 'total' FROM ORDERS \
+        Orders.orderTotal AS 'total' FROM Orders \
         JOIN Locations ON Orders.locationID = Locations.locationID \
         JOIN Customers ON Customers.customerID = Orders.customerID \
         JOIN Employees ON Employees.employeeID = Orders.employeeID \
         ORDER BY estimate;`;
         const [orders] = await db.query(query1);
+        const query2 = `SELECT \
+            Customers.customerID AS 'id', \
+            Customers.firstName AS 'first', \
+            Customers.lastName AS 'last', \
+            Customers.phone AS 'phone', \
+            Customers.email AS 'email' FROM Customers;`
+        const [customers] = await db.query(query2);
+        const query3 = `SELECT \
+            Products.productID AS 'ID', \
+            Products.productName AS 'name', \
+            Products.productType AS 'type', \
+            Products.unitPrice AS 'price' \ 
+            FROM Products;`;
+        const [products] = await db.query(query3);;
+        const query4 = `SELECT  \
+            Employees.employeeID as 'id', \
+            Employees.firstName as 'first', \
+            Employees.lastName as 'last' \
+            FROM Employees;`;
+        const [employees] = await db.query(query4);
 
         // Render the Customer Table inside of the customer.hbs file
-        res.render('orders', { orders: orders});
-=======
-            Orders.orderID          AS orderID,
-            Orders.customerID       AS customerID,
-            Orders.employeeID       AS employeeID,
-            Orders.locationID       AS locationID,
-            Orders.address          AS address,
-            Orders.dateOrdered      AS dateOrdered,
-            Orders.dateEstimateDelivery AS dateEstimateDelivery,
-            Orders.dateDelivered    AS dateDelivered,
-            Orders.orderStatus      AS orderStatus,
-            Orders.subtotal         AS subtotal,
-            Orders.tax              AS tax,
-            Orders.orderTotal       AS orderTotal
-        FROM Orders
-        ORDER BY Orders.dateEstimateDelivery;`;
-        const [orders] = await db.query(query1);
-
-        const formatted = orders.map(o => ({
-        ...o,
-        dateOrdered: o.dateOrdered.toISOString().split('T')[0],
-        dateEstimateDelivery: o.dateEstimateDelivery.toISOString().split('T')[0],
-        dateDelivered: o.dateDelivered
-            ? o.dateDelivered.toISOString().split('T')[0]
-            : null
-        }));
-
-
-        // Render the Customer Table inside of the customer.hbs file
-        res.render('orders', { orders: formatted});
->>>>>>> 185142f (Initial commit on sbleyl branch)
+        res.render('orders', { orders: orders, customers:customers, products:products, employees:employees});
     } catch (error) {
         console.error('Error executing queries:', error);
         // Send a generic error message to the browser
@@ -252,24 +192,51 @@ app.get('/orders', async function (req, res) {
     }
 });
 
-//Products Ordered table -not yet implemented but the easiest way will be as a 'view button' from the order
-//select Product.productName, productsOrdered.quantity, productsOrdered.productprice as 'indprice', productsordered.totalproductprice as 'totalproduct'  
+
 //retrieve a list of orders- requires joins
-app.get('/orders/:id', async function (req, res) {
+app.get('/invoices', async function (req, res) {
     try {
         // returns relevant attributes on locations
         const query1 = `SELECT  \
+        ProductsOrdered.orderID as 'ID', \
+        Products.productName, \
+        ProductsOrdered.quantity, \
+        ProductsOrdered.productprice as 'indprice', \
+        ProductsOrdered.totalproductprice as 'totalproduct' \
+        FROM Products \
+        JOIN ProductsOrdered ON ProductsOrdered.productID = Products.productID;`;
+        const [invoice] = await db.query(query1);
+
+        // Render the Customer Table inside of the customer.hbs file
+        res.render('invoices', { invoice: invoice});
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            "We're sorry, we've hit an error on our end. Please check back in a few minutes"
+        );
+    }
+});
+
+
+
+//retrieve a list of orders- requires joins
+app.get('/invoices/:id', async function (req, res) {
+    try {
+        // returns relevant attributes on locations
+        const query1 = `SELECT  \
+        ProductsOrdered.orderID as 'ID', \
         Products.productName, \
         ProductsOrdered.quantity, \
         ProductsOrdered.productprice as 'indprice', \
         ProductsOrdered.totalproductprice as 'totalproduct' \
         FROM Products \
         JOIN ProductsOrdered ON ProductsOrdered.productID = Products.productID \
-        WHERE ProductsOrdered.orderID = ${req.params.id};`;
+        WHERE ProductsOrdered.orderID = ${req.params['id']};`;
         const [invoice] = await db.query(query1);
 
         // Render the Customer Table inside of the customer.hbs file
-        res.render('invoice', { invoice: invoice});
+        res.render('invoices', { invoice: invoice});
     } catch (error) {
         console.error('Error executing queries:', error);
         // Send a generic error message to the browser
@@ -280,7 +247,32 @@ app.get('/orders/:id', async function (req, res) {
 });
 
 
-app.get('/locations/:id', async function (req, res) {
+app.get('/inventory', async function (req, res) {
+    try {
+        // returns relevant attributes on locations
+        const query1 = `SELECT  \
+        Locations.locationName as 'site', \
+        Products.productName as 'product' \
+        FROM ProductLocation \
+        JOIN Products ON ProductLocation.productID = Products.productID \
+        JOIN Locations ON ProductLocation.locationID = Locations.locationID`;
+        const [inventory] = await db.query(query1);
+
+        // Render the Customer Table inside of the customer.hbs file
+        res.render('inventory', { inventory: inventory});
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            "We're sorry, we've hit an error on our end. Please check back in a few minutes"
+        );
+    }
+});
+
+
+
+
+app.get('/inventory/:id', async function (req, res) {
     try {
         // returns relevant attributes on locations
         const query1 = `SELECT  \
@@ -302,6 +294,65 @@ app.get('/locations/:id', async function (req, res) {
         );
     }
 });
+
+app.get('/purchase', async function (req, res) {
+    try {
+        // returns relevant attributes on locations
+        const query1 = `SELECT \
+            Customers.customerID AS 'id', \
+            Customers.firstName AS 'first', \
+            Customers.lastName AS 'last', \
+            Customers.phone AS 'phone', \
+            Customers.email AS 'email' FROM Customers;`
+        const [customers] = await db.query(query1);
+        const query2 = `SELECT \
+            Products.productID AS 'ID', \
+            Products.productName AS 'name', \
+            Products.productType AS 'type', \
+            Products.unitPrice AS 'price' \ 
+            FROM Products;`;
+        const [products] = await db.query(query2);;
+        const query3 = `SELECT  \
+            Employees.employeeID as 'id', \
+            Employees.firstName as 'first', \
+            Employees.lastName as 'last' \
+            FROM Employees;`;
+        const [employees] = await db.query(query3);
+
+        // Render the Customer Table inside of the customer.hbs file
+        res.render('purchase', { customers: customers, employees: employees, products:products});
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            "We're sorry, we've hit an error on our end. Please check back in a few minutes"
+        );
+    }
+});
+
+// Reset Procedure
+app.get('/reset', async (req, res) => {
+  try {
+    await db.query(`CALL ResetFurniTech();`);
+    res.redirect('/');
+    console.log('Redirect Success')
+  } catch (error) {
+    console.error('Error resetting database:', error);
+    res.status(500).send('Database reset failed');
+  }
+});
+
+// Demo Delete for Leroy Jenkins
+app.get('/demo-delete', async (req, res) => {
+  try {
+    await db.query(`CALL DeleteLeroy();`);
+    res.redirect('/customers');
+  } catch (error) {
+    console.error('Error running demo delete:', error);
+    res.status(500).send('Demo delete failed');
+  }
+});
+
 
 
 // ########################################

7cedd2994f077cac0f51e0350cb4ab1c8854c8b1
Joshua Griep <griepj@oregonstate.edu>
2025-05-31 15:43:36 -0400

    app.js update to griep branch

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-const PORT = 2998;
+const PORT = 24007;
 
 // Database for CRUD actions on the data
 const db = require('./database/db-connector.js');
@@ -23,7 +23,7 @@ app.set('view engine', '.hbs'); // Use handlebars engine for *.hbs files.
 // ########################################
 // ########## ROUTE HANDLERS
 
-// READ ROUTES
+// Home Page
 app.get('/', async function (req, res) {
     try {
         res.render('home'); // Render the home.hbs file
@@ -35,6 +35,28 @@ app.get('/', async function (req, res) {
 });
 
 
+// call a function that resets the database to original state
+app.get('/reset', async function (req, res) {
+    try {
+        //no arguments passed in the method- get works fine
+
+        const query1 = `CALL ResetFurniTech();`;
+
+        // no output returned by procedure- await will cause timeout, and nothing to store as variable
+        db.query(query1);
+
+        console.log(`database reset`);
+
+        // Go back to the home page
+        res.redirect('/');
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
 
 //retrieve a list of our current customers
 app.get('/customers', async function (req, res) {
@@ -55,6 +77,210 @@ app.get('/customers', async function (req, res) {
         // Send a generic error message to the browser
         res.status(500).send(
             "We're sorry, we've hit an error on our end. Please check back in a few minutes"
+        );
+    }
+});
+
+
+
+//create a new customer
+app.post('/customers/new', async function (req, res) {
+    try {
+        // Parse frontend form information
+        let data = req.body;
+
+        // Create and execute our queries
+        // Using parameterized queries (Prevents SQL injection attacks)
+        const query1 = `CALL sp_CreateCustomer(?, ?, ?, ?);`;
+
+        // Store ID of last inserted row
+        const [[[rows]]] = await db.query(query1, [
+            data.create_customer_fname,
+            data.create_customer_lname,
+            data.create_customer_phone,
+            data.create_customer_email,
+        ]);
+
+        console.log(`CREATE customers. ID: ${rows.new_id} ` +
+            `Name: ${data.create_customer_fname} ${data.create_customer_lname}`
+        );
+
+        // Redirect the user to the updated webpage
+        res.redirect('/customers');
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+//Update an existing customer entity
+app.post('/customers/update', async function (req, res) {
+    try {
+        // Parse frontend form information
+        const data = req.body;
+
+        // Create and execute our query
+        // Using parameterized queries (Prevents SQL injection attacks)
+        const query1 = 'CALL sp_UpdateCustomer(?, ?, ?, ?, ?);';
+        const query2 = 'SELECT firstname, lastname, phone, email FROM Customers WHERE customerid = ?;';
+        await db.query(query1, [
+            data.update_customer_id,
+            data.update_customer_fname,
+            data.update_customer_lname,
+            data.update_customer_phone,
+            data.update_customer_email
+        ]);
+        const [[rows]] = await db.query(query2, [data.update_customer_id]);
+
+        console.log(`UPDATE customers. ID: ${data.update_customer_id} ` +
+            `Name: ${rows.firstname} ${rows.lastname}`
+        );
+
+        // Redirect the user to the updated webpage data
+        res.redirect('/customers');
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+app.post('/customers/delete', async function (req, res) {
+    try {
+        // Parse frontend form information
+        let data = req.body;
+
+        // Create and execute our query
+        // Using parameterized queries (Prevents SQL injection attacks)
+        const query1 = `CALL sp_DeleteCustomer(?);`;
+        await db.query(query1, [data.delete_customer_id]);
+
+        console.log(`DELETE customer. ID: ${data.delete_customer_id} ` +
+            `Name: ${data.delete_customer_name}`
+        );
+
+        // Redirect the user to the updated webpage data
+        res.redirect('/customers');
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+//retrieve a list of orders- requires joins
+app.get('/invoices', async function (req, res) {
+    try {
+        // returns relevant attributes on locations
+        const query1 = `SELECT  \
+        ProductsOrdered.orderID as 'ID', \
+        Products.productName, \
+        ProductsOrdered.quantity, \
+        ProductsOrdered.productprice as 'indprice', \
+        ProductsOrdered.totalproductprice as 'totalproduct' \
+        FROM Products \
+        JOIN ProductsOrdered ON ProductsOrdered.productID = Products.productID;`;
+        const [invoice] = await db.query(query1);
+
+        // Render the Customer Table inside of the customer.hbs file
+        res.render('invoices', { invoice: invoice});
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            "We're sorry, we've hit an error on our end. Please check back in a few minutes"
+        );
+    }
+});
+
+//vanity project- lets you get the invoice (orderdetails) of a specific ID
+//retrieve a list of orders- requires joins
+app.get('/invoices/:id', async function (req, res) {
+    try {
+        // returns relevant attributes on locations
+        const query1 = `SELECT  \
+        ProductsOrdered.orderID as 'ID', \
+        Products.productName, \
+        ProductsOrdered.quantity, \
+        ProductsOrdered.productprice as 'indprice', \
+        ProductsOrdered.totalproductprice as 'totalproduct' \
+        FROM Products \
+        JOIN ProductsOrdered ON ProductsOrdered.productID = Products.productID \
+        WHERE ProductsOrdered.orderID = ${req.params['id']};`;
+        const [invoice] = await db.query(query1);
+
+        // Render the Customer Table inside of the customer.hbs file
+        res.render('invoices', { invoice: invoice});
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            "We're sorry, we've hit an error on our end. Please check back in a few minutes"
+        );
+    }
+});
+
+
+
+
+app.post('/invoices/update', async function (req, res) {
+    try {
+        // Parse frontend form information
+        const data = req.body;
+
+        // Create and execute our query
+        // Using parameterized queries (Prevents SQL injection attacks)
+        const query1 = 'CALL UpdateOrderDetail(?, ?, ?);';
+        const query2 = 'SELECT firstname, lastname, phone, email FROM Customers WHERE customerid = ?;';
+        await db.query(query1, [
+            data.update_orderdetails_id,
+            data.update_orderdetails_count,
+            data.update_orderdetails_price
+        ]);
+        const [[rows]] = await db.query(query2, [data.update_orderdetails_id]);
+
+        console.log(`UPDATE Orderdetails. ID: ${data.update_orderdetails_id} `
+        );
+
+        // Redirect the user to the updated webpage data
+        res.redirect('/invoices');
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+app.post('/invoices/delete', async function (req, res) {
+    try {
+        // Parse frontend form information
+        let data = req.body;
+
+        // Create and execute our query
+        // Using parameterized queries (Prevents SQL injection attacks)
+        const query1 = `CALL DeleteOrderDetail(?);`;
+        await db.query(query1, [data.delete_orderdetail_id]);
+
+        console.log(`DELETE Order Detail. ID: ${data.delete_orderdetail_id} ` +
+            `Name: ${data.delete_orderdetail_name}`
+        );
+
+        // Redirect the user to the updated webpage data
+        res.redirect('/invoices');
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
         );
     }
 });
@@ -193,60 +419,7 @@ app.get('/orders', async function (req, res) {
 });
 
 
-//retrieve a list of orders- requires joins
-app.get('/invoices', async function (req, res) {
-    try {
-        // returns relevant attributes on locations
-        const query1 = `SELECT  \
-        ProductsOrdered.orderID as 'ID', \
-        Products.productName, \
-        ProductsOrdered.quantity, \
-        ProductsOrdered.productprice as 'indprice', \
-        ProductsOrdered.totalproductprice as 'totalproduct' \
-        FROM Products \
-        JOIN ProductsOrdered ON ProductsOrdered.productID = Products.productID;`;
-        const [invoice] = await db.query(query1);
-
-        // Render the Customer Table inside of the customer.hbs file
-        res.render('invoices', { invoice: invoice});
-    } catch (error) {
-        console.error('Error executing queries:', error);
-        // Send a generic error message to the browser
-        res.status(500).send(
-            "We're sorry, we've hit an error on our end. Please check back in a few minutes"
-        );
-    }
-});
-
-
-
-//retrieve a list of orders- requires joins
-app.get('/invoices/:id', async function (req, res) {
-    try {
-        // returns relevant attributes on locations
-        const query1 = `SELECT  \
-        ProductsOrdered.orderID as 'ID', \
-        Products.productName, \
-        ProductsOrdered.quantity, \
-        ProductsOrdered.productprice as 'indprice', \
-        ProductsOrdered.totalproductprice as 'totalproduct' \
-        FROM Products \
-        JOIN ProductsOrdered ON ProductsOrdered.productID = Products.productID \
-        WHERE ProductsOrdered.orderID = ${req.params['id']};`;
-        const [invoice] = await db.query(query1);
-
-        // Render the Customer Table inside of the customer.hbs file
-        res.render('invoices', { invoice: invoice});
-    } catch (error) {
-        console.error('Error executing queries:', error);
-        // Send a generic error message to the browser
-        res.status(500).send(
-            "We're sorry, we've hit an error on our end. Please check back in a few minutes"
-        );
-    }
-});
-
-
+//return the inventory table
 app.get('/inventory', async function (req, res) {
     try {
         // returns relevant attributes on locations
@@ -271,7 +444,7 @@ app.get('/inventory', async function (req, res) {
 
 
 
-
+//vanity project- returns the inventory at a specific location
 app.get('/inventory/:id', async function (req, res) {
     try {
         // returns relevant attributes on locations
@@ -295,6 +468,8 @@ app.get('/inventory/:id', async function (req, res) {
     }
 });
 
+//fake purchase page
+//doesn't actually work- dummy page that could create an order, and additionally an order detail
 app.get('/purchase', async function (req, res) {
     try {
         // returns relevant attributes on locations
